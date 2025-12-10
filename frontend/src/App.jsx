598945097fdbabbc963e75c5d8682ab20f0be242
tkolb613 <tkolbinger@gmail.com>
2025-12-10 11:34:01 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,11 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import GroceriesAppContainer from "./Components/GroceriesAppContainer";
-import HomePage from "./Components/HomePage.jsx";
-import CreateUser from "./Components/CreateUser.jsx";
+//import HomePage from "./Components/HomePage.jsx";
+//import CreateUser from "./Components/CreateUser.jsx";
 import ProductForm from "./Components/ProductForm.jsx";
-import NotAuthorized from "./Components/NotAuthorized.jsx";
-import PageNotFound from "./Components/PageNotFound.jsx";
+//import NotAuthorized from "./Components/NotAuthorized.jsx";
+//import PageNotFound from "./Components/PageNotFound.jsx";
 import Authorized from "./Components/Authorized.jsx";
 
 function App() {
@@ -18,8 +18,8 @@ function App() {
           <Route path="/create-user" element={<CreateUser />} />
 
           <Route path="/main" element={<Authorized><GroceriesAppContainer /></Authorized>} />
-          <Route path="/add-product" element={<Authorized><ProductForm /></Authorized>} />
-          <Route path="/edit-product/:id" element={<Authorized><ProductForm /></Authorized>} />
+          <Route path="/add-product" element={<Authorized role="admin"><ProductForm /></Authorized>} />
+          <Route path="/edit-product/:id" element={<Authorized role="admin"><ProductForm /></Authorized>} />
 
           <Route path="/not-authorized" element={<NotAuthorized />} />
           <Route path="*" element={<PageNotFound />} />
