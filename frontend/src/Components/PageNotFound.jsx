4f917823ffdfc536d6e7706207d3f193befc476d
Chad Watson <chadwatson2005@gmail.com>
2025-12-11 11:09:33 -0500

    Completed Page Not Found

--- frontend/src/Components/PageNotFound.jsx
+++ frontend/src/Components/PageNotFound.jsx
@@ -1,3 +1,9 @@
+import { Link } from "react-router-dom";
+
 export default function PageNotFound() {
-  return;
+  return <div>
+  <h1>🧐🧐</h1>
+  <h1>ERROR 404: Page Not Found</h1>
+  <Link to="/">Back to Login Page</Link>
+  </div>
 }
