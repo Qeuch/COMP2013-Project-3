533de61f7c181784a6fefe85f87714c1e6b8d598
tkolb613 <tkolbinger@gmail.com>
2025-12-11 12:50:27 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,25 +17,12 @@ function App() {
 
           <Route path="/create-user" element={<RegisterPage />} />
 
-<<<<<<< Updated upstream
-          <Route path="/add-product" element={<Authorized role="admin">
-            <ProductForm handleOnSubmit={handleOnSubmit} /></Authorized>}/>
-
-          <Route path="/edit-product/:id" element={<Authorized role="admin">
-                <ProductForm handleOnSubmit={handleOnSubmit} /></Authorized>}/>
-=======
           <Route
             // Changed this to /main/* as the component now has child routes
             path="/main/*" element={/*<Authorized role='admin'>*/<GroceriesAppContainer />/*</Authorized>*/}/>
->>>>>>> Stashed changes
 
           <Route path="/not-authorized" element={<NotAuthorized />} />
           <Route path="*" element={<PageNotFound />} />
-<<<<<<< Updated upstream
-          
-=======
-
->>>>>>> Stashed changes
         </Routes>
       </Router>
     </>
